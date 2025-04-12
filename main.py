a835438e367b504f97acf4c2d9505be0b3482885
Sameer Rao <sameerrao@169-231-59-48.wireless.ucsb.edu>
2025-04-12 15:24:22 -0700

    editing main

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ if __name__ == "__main__":
         query = input("Ask a question about the Great Gatsby: ")
         result = run_pipeline(query)
 
+        print("\n===== Query =====")
+        print(query)
+        print()
+
         print("\n===== Generated Answer =====")
         print(result['answer'].raw)
         print()
