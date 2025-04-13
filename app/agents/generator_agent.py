0b646e29419aca2e6516480bd606f427afa6939c
Sameer Rao <sameerrao@169-231-59-48.wireless.ucsb.edu>
2025-04-12 23:10:54 -0700

    removed uneccesary imports

--- app/agents/generator_agent.py
+++ app/agents/generator_agent.py
@@ -1,5 +1,4 @@
 from crewai import Agent
-from langchain_openai import ChatOpenAI
 
 #creates generator agent: generates answer based on context from retreiver agent
 def create_generator_agent():
@@ -8,6 +7,5 @@ def create_generator_agent():
         goal="Answer questions based on the Coffee articles with insight and clarity.",
         backstory="A thoughtful AI with a background in coffee.",
         tools=[],
-        llm=ChatOpenAI(model="gpt-4-0613", temperature=0),
         verbose=True
     )
--- app/agents/retriever_agent.py
+++ app/agents/retriever_agent.py
@@ -1,5 +1,4 @@
 from crewai import Agent
-from langchain_openai import ChatOpenAI
 from app.tools.gatsby_retriever_tool import GatsbyRetrieverTool
 
 #creates retreiver agent: finds relevant chunks from Qdrant based on query
@@ -9,6 +8,6 @@ def create_retriever_agent():
         goal="Find passages from the articles about Coffee relevant to any question.",
         backstory="An AI literary detective with deep knowledge of Coffee.",
         tools=[GatsbyRetrieverTool()],
-        llm=ChatOpenAI(model="gpt-4-0613", temperature=0),
+        # llm=ChatOpenAI(model="gpt-4-0613", temperature=0),
         verbose=True
     )
